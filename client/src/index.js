4b70caf80bece85a009eea750052379f9926d3a0
Chandhanu M K <chandhanu.m.k@gmail.com>
2024-04-13 16:32:19 -0400

    Added infopanel and Redux for lightweight reload - extra
    Update:
    --------
    1. Added InfoPanel, The result page is working
    2. Added Redux Layer (Extra)
    3. Added Toggle Button
    
    ToDO:
    ------
    1. Stylize WeatherApp
    2. Add City name to find the Weather
    3. Hosting (Research Needed)

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Header/>
-    <App />
-  </React.StrictMode>
+// Import Redux and Reducers
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+
+// Create the Redux Store
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <React.StrictMode>
+    <Provider store={store}>
+      <Header/>
+      <App />
+    </Provider>
+ </React.StrictMode>
+);
