2f22ea2202acf344f878be35bec397dc0ff47c14
Chandhanu M K <chandhanu.m.k@gmail.com>
2024-04-13 19:12:34 -0400

    Working Module - Fixed Bug
    During development, introduced bug, reverted to old commit and made the changes again

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers'; // Adjust the path as necessary
 import Header from "./Components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-// Import Redux and Reducers
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers';
-
-// Create the Redux Store
+// Create the Redux store
 const store = createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the app with Redux and React-Redux
 root.render(
  <React.StrictMode>
     <Provider store={store}>
@@ -25,3 +25,8 @@ root.render(
     </Provider>
  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
